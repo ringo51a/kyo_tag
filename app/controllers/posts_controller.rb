0714f485c9be57936c1f02546cf0269552d9b3b6
ringo51 <150096388+ringo51a@users.noreply.github.com>
2024-06-11 11:26:59 +0900

    add:掲示板作成画面の表示まで実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   skip_before_action :require_login, only: %i[index]
 
+  def new
+    @post = Post.new
+  end
+
   def index
     @posts = Post.includes(:user)
   end
