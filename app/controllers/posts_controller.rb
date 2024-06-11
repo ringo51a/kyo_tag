471f84b7ff7adcb203c6a31d138eec049380b5f7
ringo51 <150096388+ringo51a@users.noreply.github.com>
2024-06-11 11:56:53 +0900

    add:投稿作成処理を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,21 @@ class PostsController < ApplicationController
   def index
     @posts = Post.includes(:user)
   end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: t('defaults.flash_message.created', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :published_at)
+  end
+
 end
