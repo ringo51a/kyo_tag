dde6a6c4eaca09e24c623d60a672ee53fc52a166
ringo51 <150096388+ringo51a@users.noreply.github.com>
2024-06-12 11:36:26 +0900

    fix:画像のリサイズは後で実行することとしminimagicに関する記述は削除

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,8 @@
 class ImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
-  process resize_to_fit: [400, 200]
-  
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  # include CarrierWave::MiniMagick
+
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
