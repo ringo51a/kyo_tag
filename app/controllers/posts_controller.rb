b87de2f8c0aadd0412c8ceb0cb605b47c6ff8573
ringo51 <150096388+ringo51a@users.noreply.github.com>
2024-06-14 11:12:10 +0900

    add:投稿の編集・削除機能を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,25 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    redirect_to posts_path, status: :see_other
+  end
+
   private
 
   def post_params
