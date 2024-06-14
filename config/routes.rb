574e623c896de7a365d567f75cd4b3ee33b74c60
ringo51 <150096388+ringo51a@users.noreply.github.com>
2024-06-14 12:02:23 +0900

    add:ログインユーザーの投稿のみ編集・削除アイコンが表示されるように実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root 'static_pages#top'
   resources :users, only: %i[new create]
-  resources :posts, only: %i[index new create show rdit update destroy]
+  resources :posts, only: %i[index new create show edit update destroy]
 
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
