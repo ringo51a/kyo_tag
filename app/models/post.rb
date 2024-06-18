76c59b0b2bd5f1c95db8f68501da5a4c901bbe50
ringo51 <150096388+ringo51a@users.noreply.github.com>
2024-06-18 11:09:41 +0900

    add:tag,tagging,postモデルの編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,24 @@
 class Post < ApplicationRecord
+  belongs_to :user
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+
   validates :title, presence: true, length: { maximum: 255 }
   validates :body, presence: true, length: { maximum: 65_535 }
-      
-  belongs_to :user
 
   mount_uploader :post_image, ImageUploader
+
+  def save_with_tags(tag_names:)
+    ActiveRecord::Base.transaction do
+      self.tags = tag_names.map { |name| Tag.find_or_initialize_by(name: name.strip) }
+      save!
+    end
+    true
+  rescue StandardError
+    false
+  end
+
+  def tag_names
+    tags.map(&:name).join(',')  # pluckだと新規作成失敗時に値が残らない(返り値がnilになる)
+  end
 end
