c6b6a413b64e0da0e8b8e191535c83050ad05eda
ringo51 <150096388+ringo51a@users.noreply.github.com>
2024-06-18 12:53:26 +0900

    add:postモデルの編集、タグの保存処理を実装、タグ投稿フォームの実装、タグの表示機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,12 +6,17 @@ class PostsController < ApplicationController
   end
 
   def index
-    @posts = Post.includes(:user)
-  end
+    posts = if (tag_name = params[:tag_name])
+              Post.with_tag(tag_name)
+            else
+              Post.all
+            end
+    @posts = posts.includes(:user).order(created_at: :desc)
+  end  
 
   def create
     @post = current_user.posts.build(post_params)
-    if @post.save
+    if @post.save_with_tags(tag_names: params.dig(:post, :tag_names).split(',').uniq)
       redirect_to posts_path, success: t('defaults.flash_message.created', item: Post.model_name.human)
     else
       flash.now[:danger] = t('defaults.flash_message.not_created', item: Post.model_name.human)
@@ -29,7 +34,8 @@ class PostsController < ApplicationController
 
   def update
     @post = current_user.posts.find(params[:id])
-    if @post.update(post_params)
+    @post.assign_attributes(post_params)
+    if @post.save_with_tags(tag_names: params.dig(:post, :tag_names).split(',').uniq)
       redirect_to post_path(@post)
     else
       render :edit, status: :unprocessable_entity
